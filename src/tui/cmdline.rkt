dc42fd772cafb1cfbb99fe010085343bd5834888
Hans Sun <hanssun07@gmail.com>
2023-09-19 22:20:45 -0400

    tests and macro for cmdline parsing

--- src/tui/cmdline.rkt
+++ src/tui/cmdline.rkt
@@ -13,6 +13,7 @@
     data/union-find
     (for-syntax racket/base
         syntax/parse
+        threading
 ))
 
 (:typedef FlagLiteral String)
@@ -23,9 +24,55 @@
                            (Listof FlagSpec)))
 (:typedef FlagTable (Listof (U String FlagGroup)))
 
-#|
-(define-syntax
-|#
+(define-syntax (from-command-line stx)
+    (define-syntax-class string-or-strings
+        #:description "string or list of strings"
+        #:attributes ([strs 1])
+        (pattern k:string #:with (strs ...) #'(k))
+        (pattern (strs:string ...)))
+    (define-syntax-class flag-spec
+        #:description "specification of a flag"
+        #:attributes ([result-spec 0])
+        (pattern (flags:string-or-strings
+                  args:id ...
+                  descs:string-or-strings
+                  body ...+)
+                 #:with (arg-strs ...)
+                        (map (lambda~> syntax->datum symbol->string (datum->syntax stx _))
+                             (attribute args))
+                 #:with result-spec #'(list (list flags.strs ...)
+                                            (list descs.strs ...)
+                                            (lambda (args ...) body ...)
+                                            (list arg-strs ...))))
+    (define-splicing-syntax-class flag-or-desc-group
+        #:description "flag group or nonflag description"
+        #:attributes ([result-spec 0])
+        (pattern (~seq result-spec:str))
+        (pattern (~seq (~and (~or* #:multi #:once-each #:once-any)
+                             flag-group-type)
+                       flag:flag-spec ...+)
+                 #:with result-spec #'(list 'flag-group-type flag.result-spec ...)))
+    (syntax-parse stx
+        [(from-command-line
+          (~alt (~optional (~seq #:cmd-name name:expr)
+                           #:name "specification of command name"
+                           #:defaults ([name #'"<cmd>"]))
+                (~optional (~seq #:argv argv:expr)
+                           #:name "specification of argument sequence"
+                           #:defaults ([argv #'(current-command-line-arguments)]))
+                (~optional (~seq #:help-flags (help-flags:expr ...))
+                           #:name "specification of help flags"
+                           #:defaults ([(help-flags 1) (list #'"--help" #'"-h" #'"-?")])))
+          ...
+          parts:flag-or-desc-group
+          ...)
+         #'(parse-command-line name argv
+         ;#'(list               name argv
+                               (list parts.result-spec ...)
+                               (list help-flags ...))]))
+                
+          
+
 (struct exn:help exn ())
 (struct exn:flag:each exn (flag))
 (struct exn:flag:any  exn (group))
@@ -33,8 +80,9 @@
 
 (: parse-command-line (String (Sequenceof String) FlagTable (Listof String) -> (Listof String)))
 (define (parse-command-line cmdname argv flagtable help-flags)
-    (define help-group `(once-each
-                         (,help-flags "Show this help." ,(lambda () (raise (exn:help))) ())))
+    (define help-group `(#:once-each
+                         (,help-flags ("Show this help.")
+                          ,(lambda () (raise (exn:help "" (current-continuation-marks)))) ())))
     (define raw-table (append flagtable (list help-group)))
     (define uf-invoked (uf-new 'invoked))
     (define flag->uf (make-immutable-hash (~> raw-table
@@ -42,12 +90,12 @@
         (map cdr _)
         (apply append _)
         (map caar _)
-        (map (lambda (x) (cons x (uf-new x)))))))
+        (map (lambda (x) (cons x (uf-new x))) _))))
     (define ((sanitize-check exn-type) spec bad-flags)
         (match-define (list flags descs fn args) spec)
         (define (wrap . xs)
             (if* (uf-same-set? (hash-ref flag->uf (car flags)) uf-invoked)
-                 (raise (exn-type bad-flags))
+                 (raise (exn-type "" (current-continuation-marks) bad-flags))
             (uf-union! uf-invoked (hash-ref flag->uf (car flags)))
             (apply fn xs)))
         (list flags descs wrap args))
@@ -55,10 +103,10 @@
         (if* (string? group) group
         (match-define (cons type specs) group)
         (case type
-            [(multi) group]
-            [(once-each)
+            [(#:multi) group]
+            [(#:once-each)
              (cons type (map (sanitize-check exn:flag:each) specs (map caar specs)))]
-            [(once-any)
+            [(#:once-any)
              (for ([spec (cdr specs)])
                 (uf-union! (hash-ref flag->uf (caaar specs))
                            (hash-ref flag->uf (caar spec))))
@@ -91,15 +139,20 @@
 (: _find-flag (FlagLiteral (Listof FlagSpec) -> (? FlagSpec)))
 (define (_find-flag flag specs)
     (if* (empty? specs) #f
+;(printf "_find-flag ~s ~s\n" flag (caar specs))
     (define spec (car specs))
-    (for/or ([flaglit (car spec)]
-             #:when (equal? flaglit flag))
-        spec)))
+    (or (for/or ([flaglit (car spec)]
+                 #:when (equal? flaglit flag))
+            spec)
+        (_find-flag flag (cdr specs)))))
 
+(define (rcons x y) (reverse (cons x (reverse y))))
 (: _parse-argv ((Sequenceof String) (Listof FlagSpec) -> (Listof String))
                ((Sequenceof String) (Listof FlagSpec) (Listof FlagSpec) (Listof String)
                 -> (Listof String)))
 (define (_parse-argv argv specs [awaiting-args '()] [collected-args '()])
+;(pretty-write specs)
+;(printf "_parse-argv ~a -- ~a ~a\n" argv awaiting-args collected-args)
     ; run effects of flags as they collect their arguments
     (if* (and (not (empty? awaiting-args))
               (<= (~> awaiting-args car cadddr length) (length collected-args)))
@@ -114,7 +167,7 @@
     (if* (empty? argv) 
          (if (empty? awaiting-args)
              (reverse collected-args)
-             (raise (exn:flag:eof)))
+             (raise (exn:flag:eof "" (current-continuation-marks))))
         (define flag (car argv))
         (cond
             [(equal? "--" flag)
@@ -122,8 +175,8 @@
             [(string-prefix? flag "--")
              (define res (_find-flag flag specs))
              (if res
-                 (_parse-argv (cdr argv) specs (cons res awaiting-args) collected-args)
-                 (_parse-argv (cdr argv) specs awaiting-args (cons flag (collected-args))))]
+                 (_parse-argv (cdr argv) specs (rcons res awaiting-args) collected-args)
+                 (_parse-argv (cdr argv) specs awaiting-args (cons flag collected-args)))]
             [(and (string-prefix? flag "-") (< 2 (string-length flag)))
              (_parse-argv (append (map (lambda~> (string #\- _)) (cdr (string->list flag)))
                                   (cdr argv))
@@ -131,7 +184,7 @@
             [(string-prefix? flag "-")
              (define res (_find-flag flag specs))
              (if res
-                 (_parse-argv (cdr argv) specs (cons res awaiting-args) collected-args)
+                 (_parse-argv (cdr argv) specs (rcons res awaiting-args) collected-args)
                  (_parse-argv (cdr argv) specs awaiting-args (cons flag collected-args)))]
             [#t  (_parse-argv (cdr argv) specs awaiting-args (cons flag collected-args))])))))
                  
@@ -148,16 +201,83 @@
             (if* (empty? flagspecs)
                  (loop (cdr flaggroup-or-descs))
             (define flagspec (car flagspecs))
-            (match-define `((,lit0 ,lits ...) (,desc0 ,descs ...) _ ,args) flagspec)
+            (match-define `((,lit0 ,lits ...) (,desc0 ,descs ...) ,_ ,args) flagspec)
             (define topline
                 (list (format "~a ~a" lit0
                               (string-join (map (lambda~> (format "<~a>" _)) args) " "))
                       desc0))
-            `((,topline
-               ,@(for/list ([lit  (sequence-append lits  (in-value ""))]
+            `(,topline
+              ,@(for/list ([lit  (sequence-append lits  (in-value ""))]
                             [desc (sequence-append descs (in-value ""))])
                     (list (format "    ~a" lit) desc))
-               ,@(grouploop (cdr flagspecs))))))))))
+              ,@(grouploop (cdr flagspecs)))))))))
     (print-table table
         '(0 0) '(100 100) '(0 2) '(left left)))
     
+;(: parse-command-line (String (Sequenceof String) FlagTable (Listof String) -> (Listof String)))
+;(:typedef FlagLiteral String)
+;(:typedef FlagDesc String)
+;(:typedef (FlagSpec a ...)
+    ;(List (Listof FlagLiteral) (Listof FlagDesc) (a ... -> Void) (Listof String)))
+;(:typedef FlagGroup (List* (U 'once-each 'once-any 'multi)
+                           ;(Listof FlagSpec)))
+;(:typedef FlagTable (Listof (U String FlagGroup)))
+(module+ main
+    (require racket/pretty racket/exn)
+    (define flags-hit (make-hash))
+    (define ((printer k) . args) (hash-set! flags-hit k args))
+    (define spec `(
+        (#:once-any (("-t" "--by-id")       ("order by ascending id")              ,(printer '-t) ())
+                    (("-p" "--by-priority") ("order by descending priority score") ,(printer '-p) ()))
+        "description line"
+        (#:once-any (("-r" "--ready-only")  ("only show ready tasks")              ,(printer '-r) ())
+                    (("-b" "--blocked-only") ("only show blocked tasks")           ,(printer '-b) ()))
+        (#:multi    (("-a" "--assigned-to") ("only show tasks assigned to <user>"
+                                             "or all <user>s, when invoked more than once") ,(printer '-a) ("user")))
+        (#:once-each (("-n" "--limit")      ("limit to <n> results") ,(printer '-n) ("n")))))
+    (define (tester . argv)
+        (hash-clear! flags-hit)
+        (define res-args (parse-command-line "list-tasks" argv spec '("-h" "--help" "-?")))
+        (hash-set! flags-hit '__args res-args)
+        (pretty-write (hash->list flags-hit)))
+
+    (tester "--by-id" "--ready-only" "--assigned-to" "alice" "--limit" "20" "cpusa:")
+    (tester "-t" "-r" "-a" "alice" "-n" "20" "cpusa:")
+    (tester "-tra" "alice" "-n" "20" "cpusa:")
+    (tester "-tran" "alice" "20" "cpusa:")
+    (tester "-antr" "alice" "20" "cpusa:")
+    (tester "--" "-tran")
+    (tester "-t-" "-tran")
+    (tester "-?")
+    (define (edisplayln e) (printf "~a\n" (exn->string e)))
+    (with-handlers ([exn:fail? edisplayln])
+        (tester "-tran"))
+    (with-handlers ([exn:fail? edisplayln])
+        (tester "-tp"))
+
+    (hash-clear! flags-hit)
+    (hash-set! flags-hit '__args
+        (from-command-line
+            #:cmd-name "list-tasks" #:argv '("-tran" "alice" "20" "cpusa:")
+            #:once-any
+            [("-t" "--by-id")       "order by ascending id"
+             (hash-set! flags-hit '-t '())]
+            [("-p" "--by-priority") "order by descending priority score"
+             (hash-set! flags-hit '-p '())]
+            "description line"
+            #:once-any
+            [("-r" "--ready-only")  "only show ready tasks"
+             (hash-set! flags-hit '-r '())]
+            [("-b" "--blocked-only") "only show blocked tasks"
+             (hash-set! flags-hit '-b '())]
+            #:multi
+            [("-a" "--assigned-to") user
+             ("only show tasks assigned to <user>"
+              "or all <user>s, when invoked more than once")
+             (hash-update! flags-hit '-a (lambda (x) (cons user x)) '())]
+            #:once-each
+            [("-n" "--limit") n "limit to <n> results"
+             (hash-set! flags-hit '-n (list n))]))
+    (pretty-write (hash->list flags-hit))
+)
+        
